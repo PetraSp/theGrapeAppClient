d52f0077bfef003e9bcf23123751c9c2e78d3bd9
Rachel Renee Flores <rachelreneeflores@gmail.com>
2017-08-02 15:40:04 +0200

    add home, header, foot components

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,11 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
