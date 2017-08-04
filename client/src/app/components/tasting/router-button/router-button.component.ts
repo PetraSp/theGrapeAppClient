a62ce85d4045b471c14f498896a08ae4f8a52a1a
Alex Buchan <alex.buchan@telefonica.net>
2017-08-04 11:10:02 +0200

    routerButton component

---
+++ client/src/app/components/tasting/router-button/router-button.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-router-button',
+  templateUrl: './router-button.component.html',
+  styleUrls: ['./router-button.component.css']
+})
+export class RouterButtonComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
