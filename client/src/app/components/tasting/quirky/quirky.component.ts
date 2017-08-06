3bceb7a651dded9cc3c0a61f1cb7626a00a2620c
Rachel Renee Flores <rachelreneeflores@gmail.com>
2017-08-06 13:14:41 +0200

    add ngif to quirky submit result button

--- client/src/app/components/tasting/quirky/quirky.component.ts
+++ client/src/app/components/tasting/quirky/quirky.component.ts
@@ -1,13 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-quirky',
   templateUrl: './quirky.component.html',
   styleUrls: ['./quirky.component.css']
 })
-export class QuirkyComponent implements OnInit {
 
-  constructor() { }
+export class QuirkyComponent implements OnInit {
+  location = '';
+
+  constructor(private _router: Router) {
+    this.location = _router.url;
+  }
 
   ngOnInit() {
   }
