737e2e5c6b51b81f37ea6bcea7d5a178df950ba2
Petra Spirkova <pspirkova@gmail.com>
2017-08-07 11:50:07 +0200

    red fruit and color schema

--- client/src/app/components/tasting/color-schema/color-schema.component.ts
+++ client/src/app/components/tasting/color-schema/color-schema.component.ts
@@ -1,4 +1,7 @@
+/*jshint esversion: 6*/
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { UserNotesService } from '../../../services/user-notes.service';
 
 @Component({
   selector: 'app-color-schema',
@@ -7,9 +10,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ColorSchemaComponent implements OnInit {
 
-  constructor() { }
+  //make an array for the user input from the toggle buttons
+  floralResponse = [];
+  location = '';
+
+  constructor(private userNotes: UserNotesService, private router: Router) {
+    this.location = router.url;
+  }
 
   ngOnInit() {
   }
 
+  // define toggle function for button
+  userInputToggle(ColorSchema) {
+    //check if floral is in array
+    console.log('ColorSchema', ColorSchema);
+    const isInArray = this.floralResponse.some((userResponse) => {
+      return userResponse === ColorSchema;
+    });
+    console.log('isInArray', isInArray);
+    //if false, add
+    if (!isInArray) { this.floralResponse.push(ColorSchema); }
+    //if true, find index in array and delete
+    else {
+      const floralIndex = this.floralResponse.indexOf(ColorSchema);
+      this.floralResponse.splice(floralIndex, 1);
+    }
+    console.log('this.floralResponse', this.floralResponse);
+  }
+
+  addToUserNotesObject() {
+    console.log('Floral data submitted.', this.floralResponse);
+    let floralData = {key: 'floral', value: this.floralResponse};
+    this.userNotes.storeTastingData(floralData);
+  }
 }
