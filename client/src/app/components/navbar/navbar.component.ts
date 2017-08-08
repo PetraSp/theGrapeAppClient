b289f258a027214cc9c4d5ae68b2497f32457403
Rachel Renee Flores <rachelreneeflores@gmail.com>
2017-08-08 11:45:47 +0200

    add header styling and previous button

--- client/src/app/components/navbar/navbar.component.ts
+++ client/src/app/components/navbar/navbar.component.ts
@@ -8,16 +8,10 @@ import { Router } from '@angular/router';
 })
 export class NavbarComponent implements OnInit {
 
-<<<<<<< HEAD
   location = '';
 
   constructor(private _router: Router) {
     this.location = _router.url;
-=======
-  location;
-  constructor(private _route: Router) {
-    this.location = _route;
->>>>>>> a3cb6d909623f4f332e303b29a9dc65ec350a19e
   }
 
   ngOnInit() {
