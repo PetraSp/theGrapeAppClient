f006107dc32f91400b25508a8faa47c9fd11f036
Alex Buchan <alex.buchan@telefonica.net>
2017-08-10 20:22:00 +0200

    Add wine info to notes page

--- client/src/app/components/notes/notes.component.ts
+++ client/src/app/components/notes/notes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { UserNotesService } from '../../services/user-notes.service';
+import { ApiService } from '../../services/api.service';
 
 @Component({
   selector: 'app-notes',
@@ -9,12 +10,19 @@ import { UserNotesService } from '../../services/user-notes.service';
 export class NotesComponent implements OnInit {
 
   notes;
+  wineName;
 
-  constructor(private userNotes: UserNotesService) {}
+  constructor(
+    private userNotes: UserNotesService,
+    private api: ApiService
+  ) {}
 
   ngOnInit() {
     this.notes = this.userNotes.notes;
     console.log("NFLSHGLHSILGBLSIBGILDBGLIDBGIL", this.notes)
+    const wineObject = JSON.parse(this.api.wineName._body);
+    this.wineName = wineObject[0];
+    console.log("this.wineName", this.wineName.name);
   }
 
 }
